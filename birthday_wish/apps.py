137ac0b6f369239f428d7d06ac195e6ec3899eec
tasinkhan <tasintsn@gmail.com>
2023-09-01 12:25:03 +0600

    create model for customers

---
+++ birthday_wish/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BirthdayWishConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'birthday_wish'
---
+++ birthday_wish/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
