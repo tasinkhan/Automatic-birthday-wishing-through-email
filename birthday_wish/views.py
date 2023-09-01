43a8dc8eb210687a93f754463c654ed52108b032
tasinkhan <tasintsn@gmail.com>
2023-09-01 15:48:36 +0600

    Create view for Customer CRUP operation

--- birthday_wish/views.py
+++ birthday_wish/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import CustomerSerializer
+from .models import Customer
+
 
 # Create your views here.
+
+class CustomerAPI(ModelViewSet):
+    """
+    Using ModelViewSet instead of generic views because we just need to enter customer informations and retrieve them.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
